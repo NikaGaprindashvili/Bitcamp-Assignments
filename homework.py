8961e3f461990f8e9663df0dca0264c9ea127346
NikaGaprindashvili <127233939+NikaGaprindashvili@users.noreply.github.com>
2023-03-07 21:01:52 +0400

    Delete homework.py

--- homework.py
+++
@@ -1,5 +0,0 @@
-# Ask the person for their name and get an answer from them
-name = input ("What is your name? ").strip().title()
-
-# Say Hello and print the inputted name
-print (f"Hello, {name}, nice to meet you!")
