989e3306803491c253e836adad4c0cfe0fe3b3b6
NikaGaprindashvili <127233939+NikaGaprindashvili@users.noreply.github.com>
2023-03-07 20:46:44 +0400

    Add files via upload

---
+++ homework 3.py
@@ -0,0 +1,13 @@
+name = input("What is your name? ").title()
+
+if name == "Giorgi":
+ print ("Hello, Giorgi")
+elif name == "Sandro":
+ print ("Hello Sandro, How are you?")
+elif name == "Nika":
+ print ("Hello Nika, Have a nice day")
+elif name == "Oto":
+ print ("Hello Oto, Have a good day")
+else:
+ print ("I don't know you!")
+
