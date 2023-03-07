b4d2cb19a1d0723c6afa3b5cbff18e041ea2514a
NikaGaprindashvili <127233939+NikaGaprindashvili@users.noreply.github.com>
2023-03-07 21:04:04 +0400

    Add files via upload

---
+++ homework 2.py
@@ -0,0 +1,3 @@
+# Asks the person for their name, Person returns answer, Says Hello to the Person
+
+print ("Hello, ", input("What is your name? ").strip().title(),(","),(" nice to meet you!"), sep="")
---
+++ homework 3.py
@@ -0,0 +1,15 @@
+# Asks the Person for their name, Person returns Answer, if Person's name is same as it listed below it returns different Greetings, if not it returns i don't know you.
+
+name = input("What is your name? ").title()
+
+if name == "Giorgi":
+ print ("Hello, Giorgi")
+elif name == "Sandro":
+ print ("Hello Sandro, How are you?")
+elif name == "Nika":
+ print ("Hello Nika, Have a nice day")
+elif name == "Oto":
+ print ("Hello Oto, Have a good day")
+else:
+ print ("I don't know you!")
+
