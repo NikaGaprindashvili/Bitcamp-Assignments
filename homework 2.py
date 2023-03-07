6b75483eaeb6f4d1029a374a668416f26445d855
NikaGaprindashvili <127233939+NikaGaprindashvili@users.noreply.github.com>
2023-03-07 21:02:18 +0400

    Add files via upload

---
+++ homework 2.py
@@ -0,0 +1,3 @@
+# Asks person for the name, Person returns answer, Says Hello to the Person
+
+print ("Hello, ", input("What is your name? ").strip().title(),(","),(" nice to meet you!"), sep="")
---
+++ homework.py
@@ -0,0 +1,5 @@
+# Asks the person for their name and gets an answer from them
+name = input ("What is your name? ").strip().title()
+
+# Says Hello to the person and prints the inputted name
+print (f"Hello, {name}, nice to meet you!")
